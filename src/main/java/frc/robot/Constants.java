14590317a3f460f8cbf49fc7c6060fd7b067d6f6
Andy Ngo <andyngo2021@gmail.com>
2021-03-10 09:09:38 -0800

    added some basic code for the motor controllers off some random guide i found

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,13 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants
+{
+    public static class CAN
+    {
+        public static final int kLeftMaster = 1;
+        public static final int kRightMaster = 3;
+        public static final int kLeftSlave = 2;
+        public static final int kRightSlave = 4;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivebase extends SubsystemBase {
+  /** Creates a new Drivebase. */
+  CANSparkMax leftMaster = new CANSparkMax(Constants.CAN.kLeftMaster, MotorType.kBrushless);
+  CANSparkMax rightMaster = new CANSparkMax(Constants.CAN.kRightMaster, MotorType.kBrushless);
+  CANSparkMax leftSlave = new CANSparkMax(Constants.CAN.kLeftSlave, MotorType.kBrushless);
+  CANSparkMax rightSlave = new CANSparkMax(Constants.CAN.kRightSlave, MotorType.kBrushless);
+
+  public Drivebase()
+  {
+    leftSlave.follow(leftMaster);
+    rightSlave.follow(rightMaster);
+
+    // Invert the motors
+    leftMaster.setInverted(false);
+    rightMaster.setInverted(true);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
