9a1499a9189badfdd4b633d3ff5acba541cbedd9
8521 <whslionsrobotics@gmail.com>
2021-03-12 15:31:57 -0800

    limelight poggers

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,15 +4,33 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry ty = table.getEntry("ty");
+  NetworkTableEntry ta = table.getEntry("ta");
   public Limelight() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    // Horizontal Offset from crosshair to target (-27 degrees to 27 degrees)
+    double x = tx.getDouble(0.0);
+    // Vertical offset from crosshair to target (-20.5 degrees to 20.5 degrees)
+    double y = ty.getDouble(0.0);
+    // Target area (0% of image to 100% of image)
+    double area = ta.getDouble(0.0);
+
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
   }
 
   @Override
