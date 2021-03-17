c01eb5d34fd3b3bc233c40576c2a19c07d5f0706
Sacred <tqly103@student.hbuhsd.edu>
2021-03-17 22:01:41 +0000

    Drive to target init/execute code

--- src/main/java/frc/robot/commands/DriveToTarget.java
+++ src/main/java/frc/robot/commands/DriveToTarget.java
@@ -22,22 +22,92 @@ public class DriveToTarget extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_chooser.setDefaultoption("Default auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", M_chooser);
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute()
-  {
+  public void execute(){
+    Update_Limelight_Tracking();
+    double steer = m_Controller.getX(Hand.kRight);
+    double drive =-m_Controller.gety(Hand.kLeft);
+
+    boolean auto = m_Controller.getAButton();
+
+    steer *= 0.70;
+    drive *= 0.70
+
+    if (auto){
+      if (m_LimelightHasValidTarget){
+        m_Drive.arcadeDrive(M_LimelightDriveCommand,m_LimelightSteerCommand);
+
+      }
+      else{
+        m_Drive.arcadeDrive(0.0,0.0);
     
+      }
+    }
+    else{
+      m_Drive.arcadeDrive(,steer);
+    }
+    
+  }
+
+  public void Update_LimeLight_Tracking(){
+    //Numbers must be tuned pepesad
+
+    //look on doc to see what they control
+    final double STEER_K = 0.03;
+    final double DRIVE_K = 0.26;
+    final double DESIRED_TARGET_AREA = 13.0;
+    final double MAX_DRIVE = 0.7;
+
+    double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+    double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+    double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+    
+    if (tv < 1.0){
+      m_LimelightHasValidTarget = false;
+      m_LimelightDriveCommand = 0.0;
+      m_LimelightSteerCommand = 0.0;
+      return;
+    }
+
+    m_LimelightHasValidTarget = true;
+
+    //Start proportional steering
+    double steer_cmd = tx * STEER_K;
+    m_LimelightSteerCommand = steer_cmd;
+
+    //drive until reach desired target
+    double drive_cmd = (DESIRED_TARGET_AREA - ta) * DRIVE_K;
+
+    //MAX OUTPUT ON ACCERATION SPEED TOWARDS GOAL
+
+    if (drive_cmd > MAX_DRIVE){
+      drive_CMD = MAX_DRIVE;
+    }
+    m_LightlightDriveCommand = drive_cmd;
+
+  }
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    break;
+  }
 
   // Returns true when the command should end.
+  //Finished assuming
   @Override
-  public boolean isFinished() {
+  public boolean isFinished(){
     return false;
   }
 }
